9869b886e1b09937e166eac87929597b1a587448
jin231009 <jin.zhuang@globalpay.com>
2024-06-04 14:45:14 +0800

    update

--- index.js
+++ index.js
@@ -5,13 +5,8 @@ async function run() {
   try {
     console.log(github.context)
 
-    console.log(core.event)
+    console.log(github.event)
 
-    const members = JSON.parse(ghMembers.stdout).map((item) => item.login);
-
-    console.log("members", members);
-
-    core.setOutput("members", releaseType);
   } catch (error) {
     console.log(error);
     core.setFailed(error.message);
